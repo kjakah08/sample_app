210256afb23f3c83a7d40dcf79231eb8164b94da
Kimiebi Akah <kjakah08@gmail.com>
2013-05-26 21:54:43 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 SampleApp::Application.routes.draw do
-  
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
 
   match '/about',   to: 'static_pages#about'
   
