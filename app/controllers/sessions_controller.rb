3883d3192beae98d90fab25f4da0e507fda3b968
Kimiebi Akah <kjakah08@gmail.com>
2013-06-02 16:28:52 -0400

    Finish sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by_email(params[:session][:email].downcase)
-		if user && user.atuhenticate(params[:session][:password])
+		if user && user.authenticate(params[:session][:password])
 			sign_in user
 			redirect_to user
 		else
@@ -18,5 +18,7 @@ class SessionsController < ApplicationController
 
 
 	def destroy
+		sign_out
+		redirect_to root_url
 	end
 end
