7d3e1cfb1c434964329daa62c0d01a1e25a5705f
Kimiebi Akah <kjakah08@gmail.com>
2013-06-05 22:27:20 -0400

    Fixed delete action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User dstroyed."
-    redirect_to user_url
+    redirect_to users_url
   end
 
   private
